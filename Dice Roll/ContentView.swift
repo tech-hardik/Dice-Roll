21a17ffe4ade18cd4a05d5b143466288cb3c5322
HARDIK SHARMA <hardik.tech@outlook.com>
2023-07-07 14:27:07 +0530

    Dice Roll App Completed!! :))

--- Dice Roll/ContentView.swift
+++ Dice Roll/ContentView.swift
@@ -8,14 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var leftDiceNumber = 1
+    @State var rightDiceNumber = 1
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack{
+            Text("Dice Roll")
+                .foregroundColor(.cyan)
+                .bold()
+                .fontWeight(.black)
+                .font(.system(size: 50))
+            
+            Spacer()
+            
+            Button {
+                leftDiceNumber = Int.random(in: 1...6)
+                rightDiceNumber = Int.random(in: 1...6)
+            } label: {
+                Text("Click me to roll the dice!")
+                    .font(.title)
+            }
+            
+            Spacer()
+            
+            HStack{
+                Image("dice\(leftDiceNumber)")
+                    .resizable()
+                    .frame(width:200,height:200)
+                Image("dice\(rightDiceNumber)")
+                    .resizable()
+                    .frame(width:200,height:200)
+            }
+            .offset(y:-200)
+            .padding()
+
         }
-        .padding()
     }
 }
 
