437c39199f5f4d54784caaedd7f0add7863cf510
HARDIK SHARMA <hardik.tech@outlook.com>
2023-07-07 14:10:28 +0530

    Initial Commit

---
+++ Dice Roll/Dice_RollApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dice_RollApp.swift
+//  Dice Roll
+//
+//  Created by HARDIK SHARMA on 07/07/23.
+//
+
+import SwiftUI
+
+@main
+struct Dice_RollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
